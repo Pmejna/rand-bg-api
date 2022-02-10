79400023d50cce3569ae2597a44b468f1a354e7b
Przemyslaw Mejna <pmejna@gmail.com>
2022-02-10 20:22:46 +0000

    add typeorm and mysql, attach to docker with mysql tcp port map to 3307

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db_rand_bg_api',
+      port: 3307,
+      username: 'root',
+      password: 'dev',
+      database: 'rand_bg_api',
+      autoLoadEntities: true,
+      synchronize: true,
+  })
+,],
+  controllers: [
+    AppController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
