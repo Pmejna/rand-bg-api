71869e8978f2677d63f927449a4550d2c731858d
Przemyslaw Mejna <pmejna@gmail.com>
2022-02-10 22:48:57 +0000

    add encrypting, hash password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
@@ -11,6 +12,14 @@ export class AuthController {
     async register(
         @Body()body
     ) {
-        return this.userService.create(body);
+        const hashedPass = await bcrypt.hash(body.user_password, 12);
+        const username = body.user_username ? body.user_username : null;
+        return this.userService.create({
+            user_first_name: body.user_first_name,
+            user_last_name: body.user_last_name,
+            user_email: body.user_email,
+            user_username: username,
+            user_password: hashedPass
+        });
     }
 }
